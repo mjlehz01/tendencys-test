0fe4e45e52286d9169fa566853584d54fc39a1d3
Jesús Hernández <mjlehz@gmail.com>
2021-11-10 15:11:23 -0600

    base

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
+const path = require("path");
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    TOKEN: process.env.TOKEN,
+    URL_BACKEND: process.env.URL_BACKEND,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
